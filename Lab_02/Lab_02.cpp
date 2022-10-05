a3a19e59ad1129ea6ea9c760ace518746257a366
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-10-05 10:41:14 +0300

    coment z2

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -9,10 +9,10 @@ int main()
 	double z2;
 	cout << "a = "; cin >> a;
 	z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
-	z2 = tan(2*a) + 1 / cos(2*a);
+	//z2 = tan(2*a) + 1 / cos(2*a);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
